d04df74182ceb0d2c0ac3ddf0d1061cdffa2e59b
Yuuki Furuyama <addsict@gmail.com>
2018-03-03 01:48:01 +0900

    add func

--- cmd/gaemigrate/main.go
+++ cmd/gaemigrate/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -21,6 +23,11 @@ Options:
     --interval               Interval Second (default: 10)
     --dryrun                 Dry run
 `
+
+type GetVersionResponse struct {
+	Id           string  `json:"id"`
+	TrafficSplit float64 `json:"traffic_split"`
+}
 
 func parseRate(rate string) ([]uint64, error) {
 	ratesStr := strings.Split(rate, ",")
@@ -65,59 +72,88 @@ func main() {
 
 	log.Printf("project=%s, service=%s, version=%s, rates=%v, interval=%d\n", project, service, version, rates, interval)
 
-	progressMarks := []string{"-", "\\", "|", "/"}
-	go func() {
-		i := 0
-		for {
-			mark := progressMarks[i%len(progressMarks)]
-			fmt.Printf("Checking current serving version... %s\r", mark)
-			time.Sleep(time.Millisecond * 100)
-			i++
-		}
-	}()
-
-	out, err := exec.Command("gcloud",
+	stdout, stderr, err := execCommandWithMessage("Checking current serving version...",
+		"gcloud",
 		"app",
 		"versions",
 		"list",
 		"--project="+project,
 		"--service="+service,
-		"--filter=traffic_split=1.0 AND version.servingStatus=SERVING",
-		"--format=value(version.id)",
-	).Output()
+		"--filter=version.servingStatus=SERVING AND traffic_split>0",
+		"--format=json",
+	)
 	if err != nil {
-		fmt.Printf("failed to get current serving version: project=%s, version=%s, error=%s", project, version, err)
+		fmt.Printf("failed to get current serving version: project=%s, version=%s, error=%s", project, version, stderr)
 		os.Exit(1)
 	}
-	currentVersion := strings.TrimSuffix(string(out), "\n")
-	log.Printf("current version: %s\n", currentVersion)
 
-	fmt.Printf("CURRENT: %s\n", currentVersion)
-	fmt.Printf("TARGET: %s\n", version)
-
-	for step := 0; step < len(rates); step++ {
-		nextRate := rates[step]
-		remainRate := 100 - nextRate
-		if !dryrun {
-			out, err := exec.Command("gcloud",
-				"--project="+project,
-				"app",
-				"services",
-				"set-traffic",
-				service,
-				fmt.Sprintf("--splits=%s=%d,%s=%d", currentVersion, remainRate, version, nextRate),
-				"--split-by=ip",
-				"--quiet",
-			).CombinedOutput()
-			if err != nil {
-				// fmt.Printf("failed to set traffic: project=%s, version=%s, rate=%d, error=%s", project, version, nextRate, err)
-				// os.Exit(1)
-			}
-			fmt.Printf("%s", out)
-		}
-
-		time.Sleep(time.Second * time.Duration(interval))
+	var getVersionResponses []GetVersionResponse
+	if err = json.Unmarshal([]byte(stdout), &getVersionResponses); err != nil {
+		fmt.Printf("failed to parse current serving version: %s", err)
+		os.Exit(1)
 	}
+
+	fmt.Printf("%v\n", getVersionResponses)
+	// currentVersion := strings.TrimSuffix(stdout, "\n")
+	// log.Printf("current version: %v\n", getVersionResponses)
+
+	// fmt.Printf("CURRENT: %s\n", currentVersion)
+	// fmt.Printf("TARGET: %s\n", version)
+
+	// for step := 0; step < len(rates); step++ {
+	// nextRate := rates[step]
+	// remainRate := 100 - nextRate
+	// if !dryrun {
+	// out, err := exec.Command("gcloud",
+	// "--project="+project,
+	// "app",
+	// "services",
+	// "set-traffic",
+	// service,
+	// fmt.Sprintf("--splits=%s=%d,%s=%d", currentVersion, remainRate, version, nextRate),
+	// "--split-by=ip",
+	// "--quiet",
+	// ).CombinedOutput()
+	// if err != nil {
+	// // fmt.Printf("failed to set traffic: project=%s, version=%s, rate=%d, error=%s", project, version, nextRate, err)
+	// // os.Exit(1)
+	// }
+	// fmt.Printf("%s", out)
+	// }
+
+	// time.Sleep(time.Second * time.Duration(interval))
+	// }
 }
 
-// func
+func execCommandWithMessage(msg, name string, arg ...string) (string, string, error) {
+	ticker := printProgressingMessage(msg)
+	stdout, stderr, err := execCommand(name, arg...)
+	ticker.Stop()
+	fmt.Printf(msg) // print message without progressing mark
+	return stdout, stderr, err
+}
+
+func execCommand(name string, arg ...string) (string, string, error) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command(name, arg...)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return stdout.String(), stderr.String(), err
+}
+
+func printProgressingMessage(msg string) *time.Ticker {
+	progressMarks := []string{"-", "\\", "|", "/"}
+	ticker := time.NewTicker(time.Millisecond * 100)
+	go func() {
+		i := 0
+		for {
+			<-ticker.C
+			mark := progressMarks[i%len(progressMarks)]
+			fmt.Printf("%s %s\r", msg, mark)
+			i++
+		}
+	}()
+	return ticker
+}
